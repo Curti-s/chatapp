2cc5beda8d5f92dcf3fb10e71ac904494b88209b
Curti.s_ <matthewscurtis.dev81@gmail.com>
2019-03-03 02:54:29 +0300

    [:sparkles: Add user management and authentication system]
    - Feature to add user management and auth system for backend using
    djoser while using UserAuth.vue component to provide front-end views for
    both signUp and signIn.
    
    - djoser creates REST endpoints to handle user auth in the django
    backend.
    
    - Currently I am unable to use axios to provide ajax requests which are
    supposed to create new users who want to signUp or create tokens for
    already registered users who need to signIn.

---
+++ chatire-app/src/components/UserAuth.vue
@@ -0,0 +1,132 @@
+<template>
+<div class="container">
+    <h1 class="text-center">Welcome to Chatire!</h1>
+    <div id="auth-container" class="row">
+        <div class="col-sm-4 offset-sm-4">
+            <ul class="nav nav-tabs nav-justified" id="myTab" role="tablist">
+                <li class="nav-item"><a href="#signup" class="nav-link active" id="signup-tab" data-toggle="tab" role="tab" aria-controls="signup" aria-selected="true">
+                    Sign Up
+                </a></li>
+                <li class="nav-item"><a href="#signin" class="nav-link" id="signin-tab" data-toggle="tab" role="tab" aria-controls="signin" aria-selected="false">
+                    Sign In
+                </a></li>
+            </ul>
+
+            <div class="tab-content" id="myTabContent">
+                <div class="tab-pane fade in show active" id="signup" role="tabpanel" aria-labelledby="signup-tab">
+                    <form @submit.prevent="signUp">
+                        <div class="form-group">
+                            <input type="email" v-model="email" class="form-control" id="email" placeholder="Email Address" required>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <input type="text" v-model="username" class="form-control" id="username" placeholder="Username" required>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <input type="password" v-model="password" class="form-control" id="password" placeholder="Password" required>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input" id="toc" required>
+                                <label for="gridCheck" class="form-check-label">
+                                    Accept terms and Conditions
+                                </label>
+                            </div>
+                        </div>
+                        <button class="btn btn-block btn-primary" type="submit">Sign Up</button>
+                    </form>
+                </div>
+
+                <div class="tab-pane fade" id="signin" role="tabpanel" aria-labelledby="signin-tab">
+                    <form @submit.prevent="signIn">
+                        <div class="form-group">
+                            <input type="text" v-model="username" class="form-control" id="username" placeholder="Username">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" v-model="password" class="form-control" id="password" placeholder="Password" required>
+                        </div>
+                        <button class="btn btn-block btn-primary" type="submit">Sign In</button>
+                    </form>
+                </div>
+            
+            </div>
+
+        </div>
+    </div>        
+</div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            email: '', username: '', password: ''
+        }
+    },
+    methods: {
+        signUp() {
+
+            return new Promise(function(resolve, reject) {
+                axios({
+                    url: 'http://localhost:8000/auth/users/create',
+                    data: {
+                        email: this.email, 
+                        username: this.username, 
+                        password: this.password
+                    },
+                    method: 'POST'
+                })
+                .then(function() {
+                    alert('Your acount has been created. You will be signed in automatically!');
+                    this.signIn();
+                })
+                .catch(function(error) {
+                    reject(error);
+                    alert(error);
+                })
+            });
+            
+        },
+        signInRoutine(user) {
+            return new Promise(function(resolve, reject) {
+                axios({
+                    url: 'http://localhost:8000/auth/token/create',
+                    data: user,
+                    method: 'POST'
+                })
+                .then(function(response) {
+                    sessionStorage.setItem('authToken', response.data.auth_token);
+                    resolve(response);
+                })
+                .cath(function(error) {
+                    sessionStorage.removeItem('authToken');
+                    reject(error);
+                    alert(error);
+                });
+            })
+            
+        },
+        signIn() {
+            const {username, password} = this;
+            this.signInRoutine({username, password}).then(function() {
+                this.$router.push("/chat");
+            });
+        }
+    }
+};
+
+</script>
+
+<style scoped>
+#auth-container {
+    margin-top: 50px;
+}
+
+.tab-content {
+    padding-top: 20px;
+}
+</style>
+
